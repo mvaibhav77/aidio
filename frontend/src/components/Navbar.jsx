5b76c8d098af600b809d00b481e3ce6c01079406
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-10-27 21:09:08 +0530

    ready to start coding

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-5xl text-gray-700 font-bold">
+        Naruto is better than One Piece
+      </h1>
+    </div>
+  );
+}
+
+export default Navbar;
