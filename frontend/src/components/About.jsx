299575d5bc6762f83cf88efaeb4ddd54aa7baeed
Ryuk69 <ry609uk@gmail.com>
2023-11-01 02:03:08 +0530

    smallchange1

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -33,9 +33,6 @@ function About() {
           >
             HOW THIS WORKS
           </h2>
-          {/* <p className="mt-2 text-lg leading-8 text-gray-600">
-              Learn how to grow your business with our expert advice.
-            </p> */}
         </div>
         <div className=" mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-20 gap-y-16 border-t md:p-4 border-gray-200 pt-10 sm:mt-8 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
